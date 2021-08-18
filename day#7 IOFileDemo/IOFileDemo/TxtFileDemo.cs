55ebd86c9b9a0e0a0296fb4d93adcbb11f9a3ffd
Abhi <abhibhovar@gmail.com>
2021-08-18 20:11:19 +0530

    Modified

--- day#7 IOFileDemo/IOFileDemo/TxtFileDemo.cs
+++ day#7 IOFileDemo/IOFileDemo/TxtFileDemo.cs
@@ -11,13 +11,15 @@ namespace IOFileDemo
     {
         static void Main()
         {
+            
             // creating a text file 
-            string path = @"D:\capgemini\training\technical\C#\IOFileDemo\IOFileDemo\";
+            string path = @"D:\capgemini\training\technical\C#\day#7 IOFileDemo\IOFileDemo\";
             string fileName = "Abhishek.txt";
-            string fileName2 = "Alka.txt";
-            /*#region input chars in the file {Over write content} 
+            string fileName2 = "Sparsh.txt";
+           /*
+            #region input chars in the file {Over write content} 
             // this region was for creation of file and set the access and mode of the file
-            FileStream fileStream = new FileStream(path + fileName2, FileMode.Create, FileAccess.Write); // (<name of file>, <mode of the file we want to create>, <access level of the file write>)
+            FileStream fileStream = new FileStream(path + fileName, FileMode.Create, FileAccess.Write); // (<name of file>, <mode of the file we want to create>, <access level of the file write>)
 
             // this is for writing content in the file 
             StreamWriter sw = new StreamWriter(fileStream); // this is the object of file we just created 
@@ -25,6 +27,8 @@ namespace IOFileDemo
             sw.WriteLine(Console.ReadLine()); // this will over write every time we run the code 
             sw.Close();
             #endregion
+            
+
             
             #region Creating file and appending content 
             StreamWriter sw; // this is the object of file we just created 
@@ -42,7 +46,7 @@ namespace IOFileDemo
             Console.WriteLine("File Writing Complete");
             #endregion
             */
-
+            
             #region Reading text file using stream reader / Copying content from one file to another 
             StreamWriter sw; // this is the object of file we just created 
             bool toAppend;
@@ -71,6 +75,7 @@ namespace IOFileDemo
             writeTo.Close();
             readFrom.Close();
             #endregion
+           
         }
     }
 }
