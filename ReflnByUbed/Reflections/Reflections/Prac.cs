f5c19781ed8144779660076bfe448168527e22de
Abhi <abhibhovar@gmail.com>
2021-08-27 23:03:06 +0530

    Modified

---
+++ ReflnByUbed/Reflections/Reflections/Prac.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Reflections
+{
+    class Prac
+    {
+        private static int num = 10;
+
+        public int Num {
+            get { return num; }
+            set { num = value; } 
+        }
+
+        static void Main()
+        {
+            Type mirror = typeof(Prac);
+            object obj = Activator.CreateInstance(typeof(Prac));
+            PropertyInfo[] props = mirror.GetProperties();
+
+            object newVal = 99;
+            Console.WriteLine(mirror.GetProperty("Num").GetIndexParameters());
+            mirror.GetProperty("Num").SetValue(obj, newVal);
+            Console.WriteLine(num);
+        }
+    }
+}
